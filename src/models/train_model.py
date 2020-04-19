349d4600ae045444e975910fa441f5a63b5fba8a
paola-md <paola.domen@gmail.com>
2020-04-19 18:01:56 -0500

    Docker con spark y orquestador con spark

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -12,12 +12,12 @@ from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from pyspark.ml.classification import LogisticRegression, DecisionTreeClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator, BinaryClassificationEvaluator
 from pyspark.mllib.evaluation import MulticlassMetrics
-    
+
 from collections import defaultdict
 
 import pandas as pd
 
-    
+
 def ignore_list(df, data_types):
     counts_summary = df.agg(*[countDistinct(c).alias(c) for c in data_types["StringType"]])
     counts_summary = counts_summary.toPandas()
@@ -36,11 +36,11 @@ def get_data_types(df):
     return data_types
 
 def create_pipeline(df, ignore):
-    # Esto lo ponemos aqui para poder modificar las 
+    # Esto lo ponemos aqui para poder modificar las
     #variables de los estimadores/transformadores
-    data_types = get_data_types(df)    
+    data_types = get_data_types(df)
     #--------------------------------------
-    
+
     # -------------- STRING --------------
     strings_used = [var for var in data_types["StringType"] if var not in ignore]
 
@@ -50,11 +50,11 @@ def create_pipeline(df, ignore):
 
     # -------------- INTEGERS --------------
     numericals_int = [var for var in data_types["IntegerType"] if var not in ignore]
-    
+
     for c in numericals_int:
         df = df.withColumn(c, df[c].cast(DoubleType()))
         df = df.withColumn(c, df[c].cast("double"))
-        
+
     numericals_int_imputed = [var + "_imputed" for var in numericals_int]
     # =======================================
 
@@ -67,10 +67,10 @@ def create_pipeline(df, ignore):
     stage_one_hot = [OneHotEncoder(inputCol= c+"_string_encoded", outputCol= c+ "_one_hot") for c in strings_used]
 
     # =============== IMPUTADORES ====================
-    stage_imputer_double = Imputer(inputCols = numericals_double, 
-                                   outputCols = numericals_double_imputed) 
-    stage_imputer_int = Imputer(inputCols = numericals_int, 
-                                outputCols = numericals_int_imputed) 
+    stage_imputer_double = Imputer(inputCols = numericals_double,
+                                   outputCols = numericals_double_imputed)
+    stage_imputer_int = Imputer(inputCols = numericals_int,
+                                outputCols = numericals_int_imputed)
 
     # ============= VECTOR ASESEMBLER ===============
     features =  numericals_double_imputed \
@@ -78,7 +78,7 @@ def create_pipeline(df, ignore):
     stage_assembler = VectorAssembler(inputCols = features, outputCol= "assem_features")
 
     # ==================== SCALER =======================
-    stage_scaler = StandardScaler(inputCol= stage_assembler.getOutputCol(), 
+    stage_scaler = StandardScaler(inputCol= stage_assembler.getOutputCol(),
                                   outputCol="scaled_features", withStd=True, withMean=True)
 
     # ================== PIPELINE ===================
@@ -88,14 +88,14 @@ def create_pipeline(df, ignore):
                                stage_assembler,                         # Assembling data
                                stage_scaler,                            # Standardize data
                           ])
-                          
+
     ## Tenemos que regesar el df porque las variables int las combierte en double
     return  pipeline , df
 
 
 def imputa_categoricos(df, ignore,data_types):
     strings_used = [var for var in data_types["StringType"] if var not in ignore]
-    
+
     missing_data_fill = {}
     for var in strings_used:
         missing_data_fill[var] = "missing"
@@ -104,24 +104,24 @@ def imputa_categoricos(df, ignore,data_types):
     return df
 
 
-    
-def add_ids(X_train, X_test, y_train, y_test):  
+
+def add_ids(X_train, X_test, y_train, y_test):
     X_train = X_train.withColumn("id", monotonically_increasing_id())
     X_test = X_test.withColumn("id", monotonically_increasing_id())
     y_train = y_train.withColumn("id", monotonically_increasing_id())
     y_test = y_test.withColumn("id", monotonically_increasing_id())
-        
+
     return X_train, X_test, y_train, y_test
 
 
 
 def get_models_params():
-    stage_pca = PCA(k = 15,inputCol = "scaled_features", 
+    stage_pca = PCA(k = 15,inputCol = "scaled_features",
                         outputCol = "features")
-    
-        
+
+
     lr = LogisticRegression()
-    
+
     lr_paramGrid = ParamGridBuilder() \
     .addGrid(stage_pca.k, [1]) \
     .addGrid(lr.maxIter, [1]) \
@@ -136,7 +136,7 @@ def get_models_params():
 
     paramGrid_list = [lr_paramGrid, dt_paramGrid]
     model_list = [lr,dt]
-    
+
     return model_list,paramGrid_list
 
 def evaluate(predictionAndLabels):
@@ -144,7 +144,7 @@ def evaluate(predictionAndLabels):
 
     # Show Validation Score (AUROC)
     evaluator = BinaryClassificationEvaluator(metricName='areaUnderROC')
-    log['AUROC'] = "%f" % evaluator.evaluate(predictionAndLabels)    
+    log['AUROC'] = "%f" % evaluator.evaluate(predictionAndLabels)
     print("Area under ROC = {}".format(log['AUROC']))
 
     # Show Validation Score (AUPR)
@@ -173,10 +173,10 @@ def evaluate(predictionAndLabels):
         log[label] = {}
         log[label]['precision'] = "%s" % metrics.precision(label)
         log[label]['recall'] = "%s" % metrics.recall(label)
-        log[label]['F1 Measure'] = "%s" % metrics.fMeasure(label, 
+        log[label]['F1 Measure'] = "%s" % metrics.fMeasure(label,
                                                            beta=0.5)
         print("[Class %s]\tprecision = %s | recall = %s | F1 Measure = %s" \
-                  % (label, log[label]['precision'], 
+                  % (label, log[label]['precision'],
                     log[label]['recall'], log[label]['F1 Measure']))
 
     return log
@@ -184,11 +184,19 @@ def evaluate(predictionAndLabels):
 def cv_stats(cvModel):
     bestModel =  cvModel.bestModel
     print("iter", bestModel.stages[-1]._java_obj.getMaxIter())
-    
 
 
-def get_data():
-    direccion = "./../data/raw/prueba.csv"
+
+def get_data(luigi):
+    #El parametro luigi es True si se corre en luigi (y docker)
+    import os
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    print(dir_path)
+
+    if luigi:
+        direccion =  "/home/data/raw/prueba.csv"
+    else:
+        direccion = "./../../data/raw/prueba.csv"
 
 
     spark = SparkSession \
@@ -197,14 +205,14 @@ def get_data():
         .config("spark.some.config.option", "some-value") \
         .getOrCreate()
 
-    df = spark.read.csv(direccion, header="true", inferSchema="true").limit(30000)
+    df = spark.read.csv(direccion, header="true", inferSchema="true").limit(20000)
     df = clean(df)
     return df
 
 
 def get_processed_train_test(df):
     data_types = get_data_types(df)
-    ignore =   ignore_list(df, data_types) 
+    ignore =   ignore_list(df, data_types)
     illegal = [s for s in df.columns if "del" in s]
     extra_illegal = ['cancelled', 'rangoatrasohoras']
     legal = [var for var in df.columns if (var not in ignore and var not in illegal and var not in extra_illegal)]
@@ -227,11 +235,41 @@ def get_processed_train_test(df):
     X_train, X_test, y_train, y_test = add_ids(X_train, X_test, y_train, y_test)
     return X_train, X_test, y_train, y_test
 
-def init_data_luigi():
-    df = get_data()
+def init_data_luigi(luigi):
+    df = get_data(luigi)
     df = clean(df)
     X_train, X_test, y_train, y_test = get_processed_train_test(df)
     return X_train, X_test, y_train, y_test
+
+
+
+def gridSearch_bins(X_train, X_test, y_train, y_test, objetivo):
+    y_test = y_test.withColumn("label",  when(y_test.rangoatrasohoras == objetivo, 1.0).otherwise(0.0))
+    y_train = y_train.withColumn("label",  when(y_train.rangoatrasohoras == objetivo, 1.0).otherwise(0.0))
+
+    df_train = X_train.join(y_train, "id", "outer").drop("id")
+    df_test = X_test.join(y_test, "id", "outer").drop("id")
+
+    stage_pca = PCA(k = 15,inputCol = "scaled_features",
+                            outputCol = "features")
+
+
+    # BUSCA EL MEJOR MODELO
+    model_list, paramGrid_list  = get_models_params()
+    for clr_model, params in zip(model_list, paramGrid_list):
+        print("Modelo evaluado: ", clr_model, "con params: ", params)
+        pipeline = Pipeline(stages= [stage_pca, clr_model])
+
+        crossval = CrossValidator(estimator=pipeline,
+                                  estimatorParamMaps=params,
+                                  evaluator=MulticlassClassificationEvaluator(metricName='f1'),
+                                  numFolds=2)  # use 3+ folds in practice
+
+        cvModel  = crossval.fit(df_train)
+        prediction = cvModel.transform(df_test)
+        evaluate(prediction)
+
+
 
 def main():
 
@@ -239,38 +277,12 @@ def main():
     df = clean(df)
 
     X_train, X_test, y_train, y_test = get_processed_train_test(df)
-    
+
     lista_objetivos = df.select('rangoatrasohoras').distinct().rdd.map(lambda r: r[0]).collect()
 
-    
+
     # CREA 4 MODELOS
     for objetivo in lista_objetivos:
         print("objetivo: ", objetivo)
 
-        y_test = y_test.withColumn("label",  when(y_test.rangoatrasohoras == objetivo, 1.0).otherwise(0.0))
-        y_train = y_train.withColumn("label",  when(y_train.rangoatrasohoras == objetivo, 1.0).otherwise(0.0))
-
-        df_train = X_train.join(y_train, "id", "outer").drop("id")
-        df_test = X_test.join(y_test, "id", "outer").drop("id")
-
-        stage_pca = PCA(k = 15,inputCol = "scaled_features", 
-                                outputCol = "features")
-
-    
-        # BUSCA EL MEJOR MODELO
-        model_list, paramGrid_list  = get_models_params()
-        for clr_model, params in zip(model_list, paramGrid_list):
-            print("Modelo evaluado: ", clr_model, "con params: ", params)
-            pipeline = Pipeline(stages= [stage_pca, clr_model])
-
-            crossval = CrossValidator(estimator=pipeline,
-                                      estimatorParamMaps=params,
-                                      evaluator=MulticlassClassificationEvaluator(metricName='f1'),
-                                      numFolds=2)  # use 3+ folds in practice
-
-            cvModel  = crossval.fit(df_train)
-            prediction = cvModel.transform(df_test)
-            evaluate(prediction)
-
-main()
-    
+        gridSearch_bins(X_train, X_test, y_train, y_test, objetivo)
--- src/orquestador-modelling.py
+++ src/orquestador-modelling.py
@@ -1,4 +1,4 @@
-# PYTHONPATH='.' AWS_PROFILE=dpa luigi --module orquestador RunTables --local-scheduler --filename metada_extract.sql --update-id 4
+# PYTHONPATH='.' AWS_PROFILE=dpa luigi --module orquestador-modelling CreateModelCancelled --local-scheduler
 
 import luigi
 import luigi.contrib.s3
@@ -16,19 +16,19 @@ from luigi.contrib.postgres import CopyToTable,PostgresQuery
 CURRENT_DIR = os.getcwd()
 # ===============================
 
-class DataLocalStorage(): 
-    def __init__(self, X_train, X_test, y_train, y_test):
-        self.X_train = None
-        self.X_test = None
-        self.y_test = None
-        self.y_train = None
-        
+class DataLocalStorage():
+    def __init__(self, X_train= None, X_test= None, y_train= None, y_test= None):
+        self.X_train =X_train
+        self.X_test =  X_test
+        self.y_test =  y_train
+        self.y_train = y_test
+
     def get_sets():
         return self.X_train, self.X_test, self.y_train, self.y_test
-    
+
 CACHE = DataLocalStorage()
-    
-    
+
+
 class GetDataSets(luigi.Task):
 
     def output(self):
@@ -36,22 +36,23 @@ class GetDataSets(luigi.Task):
         return luigi.local_target.LocalTarget(dir)
 
     def run(self):
-        X_train, X_test, y_train, y_test = init_data_luigi()
+        X_train, X_test, y_train, y_test = init_data_luigi(True)
         CACHE.X_train = X_train
         CACHE.X_test = X_test
         CACHE.y_train = y_train
         CACHE.y_test = y_test
-        
+
         z = "Obtiene Datos"
         with self.output().open('w') as output_file:
             output_file.write(z)
 
 class CreateModelCancelled(luigi.Task):
-    
+
     def requires(self):
         return GetDataSets()
-    
+
     def output(self):
+        # Chance y aqui podemos guardar el mejor modelo en una S3
         dir = CURRENT_DIR + "/target/model_cancelled.txt"
         return luigi.local_target.LocalTarget(dir)
 
@@ -60,9 +61,7 @@ class CreateModelCancelled(luigi.Task):
         X_train, X_test, y_train, y_test = CACHE.get_sets()
 
         gridSearch_bins(X_train, X_test, y_train, y_test, objetivo)
-        
+
         z = str(objetivo)
         with self.output().open('w') as output_file:
             output_file.write(z)
-
-

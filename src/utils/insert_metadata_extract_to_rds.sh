78669a831a6f67a2d390d384f1b1743937f60841
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-04-24 08:10:07 -0500

    cambio de mensaje de exito tras insertar a RDS

--- src/utils/insert_metadata_extract_to_rds.sh
+++ src/utils/insert_metadata_extract_to_rds.sh
@@ -11,4 +11,4 @@
 
 PGPASSWORD=$password psql -U $user -h $host -d $db_name -c "\COPY raw.rita FROM 'data.csv'  WITH CSV HEADER;"
 
-echo "Insersion de datos a esquema raw.rita terminada"
+echo "Data insertion on raw.rita completed"

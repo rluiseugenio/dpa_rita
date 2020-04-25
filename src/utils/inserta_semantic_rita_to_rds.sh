4ced6443afb4bbf9a05024dc2c0cd7c3558795e4
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-04-25 18:14:18 -0500

    propuesta para subir datos de csv a semantic.rita

---
+++ src/utils/inserta_semantic_rita_to_rds.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Progama que hace la insersion de datos desde data.csv hacia raw.rita
+# Nota: necesita que el archio credentials_psql.txt este presente, el cual
+# contiene los datos que permite la conezion a psql con el RDS
+# user = MY_USER
+# password = MY_PASS
+# db_name = MY_DB
+# host = MY_HOST
+
+. credentials_psql.txt
+
+# Correcion para evitar errores de importacion con psql
+cat semantic.csv | sed 's/\"\"/\N/g' > semantic.csv
+
+# importa datos hacia psql
+PGPASSWORD=$password psql -U $user -h $host -d $db_name -c "\COPY semantic.rita FROM 'data.csv'  WITH CSV HEADER;"
+
+echo "Data insertion on semantic.rita completed"

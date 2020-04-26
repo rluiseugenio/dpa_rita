22108b7da3e8bc05d0cfd1fd6b08df4c095393da
Danahirmt <51727910+Danahirmt@users.noreply.github.com>
2020-04-25 20:35:53 -0500

    Ya corre en alter_orq :)

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -6,6 +6,7 @@ from pyspark.sql.functions import col, lower, regexp_replace, split
 import psycopg2 as pg
 import pandas.io.sql as psql
 import pandas as pd
+from pyspark.sql.types import StructType, StructField, StringType
 
 
 
@@ -43,23 +44,123 @@ def clean(df):
 
 def get_data():
     
+   schema = StructType([StructField('year', StringType(), True),
+					 StructField('quarter', StringType(), True),
+                     StructField('month', StringType(), True),
+                     StructField('dayofmonth', StringType(), True),
+                     StructField('dayofweek', StringType(), True),
+                     StructField('flightdate', StringType(), True),
+                     StructField('reporting_airline', StringType(), True),
+                     StructField('dot_id_reporting_airline', StringType(), True),
+                     StructField('iata_code_reporting_airline', StringType(), True),
+                     StructField('tail_number', StringType(), True),
+                     StructField('flight_number_reporting_airline', StringType(), True),
+                     StructField('originairportid', StringType(), True),
+                     StructField('originairportseqid', StringType(), True),
+                     StructField('origincitymarketid', StringType(), True),
+                     StructField('origin', StringType(), True),
+                     StructField('origincityname', StringType(), True),
+                     StructField('originstate', StringType(), True),
+                     StructField('originstatefips', StringType(), True),
+                     StructField('originstatename', StringType(), True),
+                     StructField('originwac', StringType(), True),
+                     StructField('destairportid', StringType(), True),
+                     StructField('destairportseqid', StringType(), True),
+                     StructField('destcitymarketid', StringType(), True),
+                     StructField('dest', StringType(), True),
+                     StructField('destcityname', StringType(), True),
+                     StructField('deststate', StringType(), True),
+                     StructField('deststatefips', StringType(), True),
+                     StructField('deststatename', StringType(), True),
+                     StructField('destwac', StringType(), True),
+                     StructField('crsdeptime', StringType(), True),
+                     StructField('deptime', StringType(), True),
+                     StructField('depdelay', StringType(), True),
+                     StructField('depdelayminutes', StringType(), True),
+                     StructField('depdel15', StringType(), True),
+                     StructField('departuredelaygroups', StringType(), True),
+                     StructField('deptimeblk', StringType(), True),
+                     StructField('taxiout', StringType(), True),
+                     StructField('wheelsoff', StringType(), True),
+                     StructField('wheelson', StringType(), True),
+                     StructField('taxiin', StringType(), True),
+                     StructField('crsarrtime', StringType(), True),
+                     StructField('arrtime', StringType(), True),
+                     StructField('arrdelay', StringType(), True),
+                     StructField('arrdelayminutes', StringType(), True),
+                     StructField('arrdel15', StringType(), True),
+                     StructField('arrivaldelaygroups', StringType(), True),
+                     StructField('arrtimeblk', StringType(), True),
+                     StructField('cancelled', StringType(), True),
+                     StructField('cancellationcode', StringType(), True),
+                     StructField('diverted', StringType(), True),
+                     StructField('crselapsedtime', StringType(), True),
+                     StructField('actualelapsedtime', StringType(), True),
+                     StructField('airtime', StringType(), True),
+                     StructField('flights', StringType(), True),
+                     StructField('distance', StringType(), True),
+                     StructField('distancegroup', StringType(), True),
+                     StructField('carrierdelay text', StringType(), True),
+                     StructField('weatherdelay text', StringType(), True),
+                     StructField('nasdelay text', StringType(), True),
+                     StructField('securitydelay text', StringType(), True),
+                     StructField('lateaircraftdelay text', StringType(), True),
+                     StructField('firstdeptime', StringType(), True),
+                     StructField('totaladdgtime', StringType(), True),
+                     StructField('longestaddgtime', StringType(), True),
+                     StructField('divairportlandings', StringType(), True),
+                     StructField('divreacheddest', StringType(), True),
+                     StructField('divactualelapsedtime ', StringType(), True),
+                     StructField('divarrdelay', StringType(), True),
+                     StructField('divdistance', StringType(), True),
+                     StructField('div1airport', StringType(), True),
+                     StructField('div1airportid', StringType(), True),
+                     StructField('div1airportseqid', StringType(), True),
+                     StructField('div1wheelson', StringType(), True),
+                     StructField('div1totalgtime', StringType(), True),
+                     StructField('div1longestgtime', StringType(), True),
+                     StructField('div1wheelsoff', StringType(), True),
+                     StructField('div1tailnum', StringType(), True),
+                     StructField('div2airport', StringType(), True),
+                     StructField('div2airportid', StringType(), True),
+                     StructField('div2airportseqid', StringType(), True),
+                     StructField('div2wheelson', StringType(), True),
+                     StructField('div2totalgtime', StringType(), True),
+                     StructField('div2longestgtime', StringType(), True),
+                     StructField('div2wheelsoff', StringType(), True),
+                     StructField('div2tailnum', StringType(), True),
+                     StructField('div3airport', StringType(), True),
+                     StructField('div3airportid', StringType(), True),
+                     StructField('div3airportseqid', StringType(), True),
+                     StructField('div3wheelson', StringType(), True),
+                     StructField('div3totalgtime', StringType(), True),
+                     StructField('div3longestgtime', StringType(), True),
+                     StructField('div3wheelsoff', StringType(), True),
+                     StructField('div3tailnum', StringType(), True),
+                     StructField('div4airport', StringType(), True),
+                     StructField('div4airportid', StringType(), True),
+                     StructField('div4airportseqid', StringType(), True),
+                     StructField('div4wheelson', StringType(), True),
+                     StructField('div4totalgtime', StringType(), True),
+                     StructField('div4longestgtime', StringType(), True),
+                     StructField('div4wheelsoff', StringType(), True),
+                     StructField('div4tailnum', StringType(), True),
+                     StructField('div5airport', StringType(), True),
+                     StructField('div5airportid', StringType(), True),
+                     StructField('div5airportseqid', StringType(), True),
+                     StructField('div5wheelson', StringType(), True),
+                     StructField('div5totalgtime', StringType(), True),
+                     StructField('div5longestgtime', StringType(), True),
+                     StructField('div5wheelsoff', StringType(), True),
+                     StructField('div5tailnum', StringType(), True),
+                     StructField('fffff', StringType(), True)
+                     ])
+   connection = pg.connect("host='MY_HOST' dbname='MY_DB' user='MY_USER' password='MY_PASS'")
+   pdf = pd.read_sql_query('select * from raw.rita_light',con=connection)
+   spark = SparkSession.builder.config('spark.driver.extraClassPath', 'postgresql-9.4.1207.jar').getOrCreate()
+   df = spark.createDataFrame(pdf, schema=schema)
 
-    spark = SparkSession \
-        .builder \
-        .appName("Python Spark SQL basic example") \
-        .config("spark.jars", "./postgresql-9.4.1207.jar") \
-        .getOrCreate()
-
-    df = spark.read \
-        .format("jdbc") \
-        .option("url", "jdbc:postgresql://MY_HOST/MY_DB") \
-        .option("dbtable", "raw.rita_light") \
-        .option("user", "MY_USER") \
-        .option("password", "MY_PASS") \
-        .option("driver", "org.postgresql.Driver") \
-        .load()
-
-    return df
+   return df
 
 def init_data_luigi():
     df = get_data()

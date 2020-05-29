bc10e7bda24709c6424d50bfdc47e7e594842f75
paola-md <paola.domen@gmail.com>
2020-05-28 23:35:38 -0500

    unit tests predict

--- src/orquestadores/predictions.py
+++ src/orquestadores/predictions.py
@@ -2,7 +2,7 @@
 SIMPLE
 PYTHONPATH='.' AWS_PROFILE=dpa luigi --module predictions CreatePredictions --local-scheduler
 
-PYTHONPATH='.' AWS_PROFILE=dpa luigi --module predictions  PredictTestCols --local-scheduler
+
 '''
 from datetime import date, datetime
 from io import StringIO
@@ -18,6 +18,7 @@ from src.orquestadores.bias import EvaluateBias
 from src.models.predict_model import save_predictions
 from src.utils.db_utils import execute_query
 from src.unit_tests.predict_columns import TestPredictColumns
+from src.unit_tests.predict_types import TestColumnsTypes
 
 from src import (
     MY_USER ,
@@ -29,27 +30,71 @@ from src import (
 
 class CreatePredictions(PostgresQuery):
 
-    #def requires(self):
-    #    return EvaluateBias()
+    def requires(self):
+        return PredictTestType()
 
     user = MY_USER
     password = MY_PASS
     database = MY_DB
     host = MY_HOST
     table ='metadatos.predictions'
-    #update_id = "45" #Para que vuelva a correr
+    update_id = "46" #Para que vuelva a correr
 
     data_list =  save_predictions()
     query = "insert into " + table + " values "  + str(tuple(data_list))
 
+
+class PredictTestType(luigi.Task):
+    task_complete = False
+    '''
+    Prueba unitaria de estructura de archivos descargados
+    '''
+    def requires(self):
+        return PredictTestCols()
+
+    def run(self):
+        try:
+            unit_test = TestColumnsTypes()
+            unit_test.check_column_types()
+        except:
+            raise ValueError('types of columns do not match.')
+        finally:
+            self.task_complete = True
+
+    def complete(self):
+    # Make sure you return false when you want the task to run.
+    # And true when complete
+        return  self.task_complete
+
+@PredictTestType.event_handler(Event.SUCCESS)
+def on_success(self):
+    today = date.today()
+    d1 = today.strftime("%d%m%Y")
+
+    data_list = [d1, "check_columns_types", "success", "none"]
+    table ='metadatos.testing_predict_types'
+    query = "insert into " + table + " values "  + str(tuple(data_list))
+    execute_query(query)
+
+
+@PredictTestType.event_handler(Event.FAILURE)
+def on_failure(self,exception):
+    today = date.today()
+    d1 = today.strftime("%d%m%Y")
+
+    data_list = [d1, "check_columns_types", "failure", "type of columns do not match"]
+    table ='metadatos.testing_predict_types'
+    query = "insert into " + table + " values "  + str(tuple(data_list))
+    execute_query(query)
 
 
 class PredictTestCols(luigi.Task):
     '''
     Prueba unitaria de estructura de archivos descargados
     '''
+    task_complete = False
     #def requires(self):
-    #    return Extraction()
+    #    return EvaluateBias()
 
     def run(self):
         try:
@@ -57,7 +102,13 @@ class PredictTestCols(luigi.Task):
             unit_test.check_columns()
         except:
             raise ValueError('number of columns do not match.')
+        finally:
+            self.task_complete = True
 
+    def complete(self):
+    # Make sure you return false when you want the task to run.
+    # And true when complete
+        return  self.task_complete
 
 @PredictTestCols.event_handler(Event.SUCCESS)
 def on_success(self):
---
+++ src/unit_tests/predict_types.py
@@ -0,0 +1,60 @@
+#python -m marbles test_semantic_columns.py
+import unittest
+from marbles.mixins import mixins
+import pandas as pd
+from pandas.testing import assert_frame_equal
+import requests
+from pyspark.sql import SparkSession
+import psycopg2 as pg
+import pandas as pd
+import marbles
+import marbles.core
+from pyspark.sql.types import StructType, StructField, StringType
+#from src.features.build_features import crear_features
+from src.models.predict_model import get_predictions
+from src.utils.db_utils import get_select
+
+
+from src import(
+MY_USER,
+MY_PASS,
+MY_HOST,
+MY_PORT,
+MY_DB,
+)
+
+
+
+
+class TestColumnsTypes(marbles.core.TestCase):
+    '''
+    Verifica que la cantidad de columnas en semantic.rita
+    sean las esperadas
+
+    '''
+
+    def check_column_types(self):
+        connection = pg.connect(user=MY_USER , # Usuario RDS
+                                      password=MY_PASS, # password de usuario de RDS
+                                      host=MY_HOST ,#"127.0.0.1", # cambiar por el endpoint adecuado
+                                      port=MY_PORT, # cambiar por el puerto
+                                      database=MY_DB ) # Nombre de la base de datos
+        df = pd.read_sql_query('select * from predictions.test limit 5;',con=connection)
+        real_types = df.dtypes.to_frame()
+        dict_types = df.dtypes.to_dict()
+        print(real_types) #{'flight_number': dtype('float64'), 'distance': dtype('float64'), 'prediction': dtype('O'), 's3_name': dtype('O')}
+
+        df_pred, s3_name = get_predictions()
+
+
+        df_pandas = df_pred.toPandas()
+        df_pandas.columns = ['flight_number', 'distance','prediction', 's3_name' ]
+        df_pandas = df_pandas.astype(dict_types)
+
+        #Descomentar la siguiente línea para que falle la prueba
+        #df_pandas['distance'] = df_pandas['distance'].astype(str)
+
+        pred_types = df_pandas.dtypes.to_frame()
+        print(pred_types) #{'flight_number_reporting_airline': dtype('float64'), 'distance': dtype('O'), 'prediction': dtype('float64'), 's3_name': dtype('O')}
+
+        assert_frame_equal(real_types, pred_types)

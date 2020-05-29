6a9bfb2b45a5a0f95886dd7330ba479b524b6fb0
paola-md <paola.domen@gmail.com>
2020-05-28 21:53:37 -0500

    unit tests predict

--- src/orquestadores/predictions.py
+++ src/orquestadores/predictions.py
@@ -1,6 +1,8 @@
 '''
 SIMPLE
 PYTHONPATH='.' AWS_PROFILE=dpa luigi --module predictions CreatePredictions --local-scheduler
+
+PYTHONPATH='.' AWS_PROFILE=dpa luigi --module predictions  PredictTestCols --local-scheduler
 '''
 from datetime import date, datetime
 from io import StringIO
@@ -14,6 +16,8 @@ import pandas as pd
 
 from src.orquestadores.bias import EvaluateBias
 from src.models.predict_model import save_predictions
+from src.utils.db_utils import execute_query
+from src.unit_tests.predict_columns import TestPredictColumns
 
 from src import (
     MY_USER ,
@@ -37,3 +41,41 @@ class CreatePredictions(PostgresQuery):
 
     data_list =  save_predictions()
     query = "insert into " + table + " values "  + str(tuple(data_list))
+
+
+
+class PredictTestCols(luigi.Task):
+    '''
+    Prueba unitaria de estructura de archivos descargados
+    '''
+    #def requires(self):
+    #    return Extraction()
+
+    def run(self):
+        try:
+            unit_test = TestPredictColumns()
+            unit_test.check_columns()
+        except:
+            raise ValueError('number of columns do not match.')
+
+
+@PredictTestCols.event_handler(Event.SUCCESS)
+def on_success(self):
+    today = date.today()
+    d1 = today.strftime("%d%m%Y")
+
+    data_list = [d1, "check_columns", "success", "none"]
+    table ='metadatos.testing_predict_cols'
+    query = "insert into " + table + " values "  + str(tuple(data_list))
+    execute_query(query)
+
+
+@PredictTestCols.event_handler(Event.FAILURE)
+def on_failure(self,exception):
+    today = date.today()
+    d1 = today.strftime("%d%m%Y")
+
+    data_list = [d1, "check_columns", "failure", "number of columns do not match"]
+    table ='metadatos.testing_predict_cols'
+    query = "insert into " + table + " values "  + str(tuple(data_list))
+    execute_query(query)
---
+++ src/unit_tests/predict_columns.py
@@ -0,0 +1,37 @@
+#python -m marbles test_semantic_columns.py
+import unittest
+from marbles.mixins import mixins
+import pandas as pd
+import requests
+from pyspark.sql import SparkSession
+import psycopg2 as pg
+import pandas as pd
+import marbles
+import marbles.core
+from pyspark.sql.types import StructType, StructField, StringType
+#from src.features.build_features import crear_features
+from src.models.predict_model import get_predictions
+from src.utils.db_utils import get_select
+
+class TestPredictColumns(marbles.core.TestCase):
+    '''
+    Verifica que la cantidad de columnas en predictions.test
+    sean las esperadas
+
+    '''
+
+    def check_columns(self):
+
+        query = 'select * from predictions.test limit 1;'
+        df = get_select(query)
+
+        len_db = len(df[0])
+
+        df_pred, s3_name = get_predictions()
+        len_pred = len(df_pred.columns)
+
+        info = "Tamaño esperado " + str(len_db) + " tamaño recibido " +   str(len_pred)
+
+        # PARA QUE FALLE
+        len_pred = 2
+        self.assertEqual(len_db, len_pred, note=info)
